cef29ad7458720fc16fa36605d51edadbe8d44d6
ashiq <ashiq@dotserp.com>
2025-08-24 20:07:47 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-const http = require("http");
+const express = require("express")
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/api" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("hello");
-  } else {
-    res.writeHead(200, { "Content-Type": "text/plain" });
+const app= express();
 
-    res.end("Hello, World!\n");
-  }
-});
+app.get("",(req,res)=>{
+  res.send("hello from express")
+})
 
-server.listen(3000, "localhost", () => {
-  console.log(`Server running at http://localhost:${3000}/`);
-});
+
+app.use("",(req,res)=>{
+  res.send("test")
+})
+
+
+app.listen(3000)
+
