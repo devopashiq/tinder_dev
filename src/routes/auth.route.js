aba87ee8b0e01dd78b61ee7d2e83d3b80cbf36f2
ashiq <ashiq@dotserp.com>
2025-12-15 18:39:15 +0530

    updated config

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -34,7 +34,7 @@ router.post("/signup", async (req, res) => {
    res.cookie("token", token, {
           httpOnly: true,
           secure: false,
-          sameSite: "none",
+          sameSite: "lax",
           maxAge: 7 * 24 * 60 * 60 * 1000, 
         });
   
